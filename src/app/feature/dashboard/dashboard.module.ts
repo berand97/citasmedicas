6a69ad3a2be7a046edabe2361850bf1772d3cd36
Andrés Fabian Patiño Bermudez - Ceiba Software <andres.patino@ceiba.com.co>
2022-02-14 10:44:18 -0500

    se importa el coreModule para aceptar los componentes, en este caso para el sidebar

--- src/app/feature/dashboard/dashboard.module.ts
+++ src/app/feature/dashboard/dashboard.module.ts
@@ -6,17 +6,25 @@ import { DashboardRoutingModule } from './dashboard.routing';
 
 import { DashboardComponent } from './dashboard.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { CoreModule } from '@core/core.module';
+import { StatisticsComponent } from './pages/statistics/statistics.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { UsersComponent } from './pages/users/users.component';
 
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    ProfileComponent
+    ProfileComponent,
+    StatisticsComponent,
+    SettingsComponent,
+    UsersComponent
   ],
   imports: [
     CommonModule,
     RouterModule,
+    CoreModule,
     DashboardRoutingModule
   ]
 })
