ea246b625333cae3dab2b4fb7b37cf764b5b8061
Andrés Fabian Patiño Bermudez - Ceiba Software <andres.patino@ceiba.com.co>
2022-02-15 14:57:00 -0500

    cambios sugeridos al lanzar el lint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SecurityGuard } from '@core/guard/security.guard';
-import { HomeComponent } from '@home/home.component';
+/* import { SecurityGuard } from '@core/guard/security.guard';
+import { HomeComponent } from '@home/home.component'; */
 
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent, canActivate: [SecurityGuard]  },
-  { path: 'auth', loadChildren: ()=> import('./feature/auth/auth.module').then(mod => mod.AuthModule)},
-  { path: 'dashboard', loadChildren: ()=> import('./feature/dashboard/dashboard.module').then(mod => mod.DashboardModule)},
-  { path: 'producto', loadChildren: () => import('@producto/producto.module').then(mod => mod.ProductoModule) },
-  { path: '**', redirectTo: '/auth'},
+  { path: 'auth', loadChildren: () => import('./feature/auth/auth.module').then(mod => mod.AuthModule) },
+  { path: 'dashboard', loadChildren: () => import('./feature/dashboard/dashboard.module').then(mod => mod.DashboardModule) },
+  { path: '**', redirectTo: 'auth' },
+  /* { path: 'producto', loadChildren: () => import('@producto/producto.module').then(mod => mod.ProductoModule) }, */
+  /* { path: 'home', component: HomeComponent, canActivate: [SecurityGuard]  }, */
 
 ];
 
