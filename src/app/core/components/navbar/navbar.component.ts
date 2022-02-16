7da8d0011e3b3828f8191da1bd81bfb7f63cb770
Andrés Fabian Patiño Bermudez - Ceiba Software <andres.patino@ceiba.com.co>
2022-02-16 15:15:47 -0500

    actualizacion de unos componentes

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -3,35 +3,8 @@ import { MenuItem } from '@core/modelo/menu-item';
 
 @Component({
   selector: 'app-navbar',
-  templateUrl: 'navbar.component.html',
-  styles: [`
-    .bg-primary { background-color: var(--primary) !important; }
-
-    .bg-light { background-color: var(--light) !important; }
-
-    .bg-grey { background-color: var(--grey) !important; }
-
-    .btn-primary {
-        background-color: var(--primary);
-        border: 0;
-    }
-
-    .btn-primary:hover {
-        background-color: var(--primary);
-    }
-
-    .avatar {
-        max-width: 35px;
-    }
-
-    .btn-search {
-        right: 0;
-    }
-
-    .stat {
-        border-right: 1px solid var(--grey);
-    }
-`],
+  templateUrl: './navbar.component.html',
+  styles: [],
 })
 export class NavbarComponent implements OnInit {
 
