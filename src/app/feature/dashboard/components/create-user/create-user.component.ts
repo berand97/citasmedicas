c0f2902db7e14a76d7f44187f76a8ce5cd95b856
Andrés Fabian Patiño Bermudez - Ceiba Software <andres.patino@ceiba.com.co>
2022-02-21 17:16:37 -0500

    componente modal de creacion de Usuario

---
+++ src/app/feature/dashboard/components/create-user/create-user.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ModalGlobalService } from '@core/services/modal-global.service';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { UserService } from '../../shared/service/user.service';
+
+@Component({
+  selector: 'app-create-user',
+  templateUrl: './create-user.component.html',
+  styleUrls: ['./create-user.component.scss']
+})
+export class CreateUserComponent implements OnInit {
+
+  createUserForm = this.fb.group({
+    name: ['', [Validators.required]],
+    lastName: ['', [Validators.required]],
+    typeDocument: ['', [Validators.required]],
+    numberDocument: ['', [Validators.required]],
+    healtyCompany: ['', [Validators.required]],
+    typeAffiliate: ['', [Validators.required]],
+  })
+
+  constructor(
+    private fb: FormBuilder,
+    private modalGlobalService: ModalGlobalService,
+    private userService: UserService,
+    public activeModal: NgbActiveModal
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+
+  createUser() {
+    this.userService.createUser(this.createUserForm.value)
+      .subscribe(_resp => {
+        this.modalGlobalService.event.emit('close')
+      })
+  }
+
+
+
+}
