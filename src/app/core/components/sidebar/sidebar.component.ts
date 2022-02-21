813e2b5194386a1c9f060901a228b791ba1c01f8
Andrés Fabian Patiño Bermudez - Ceiba Software <andres.patino@ceiba.com.co>
2022-02-21 17:26:46 -0500

    cambios en la ruta y nombre del menu

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -8,13 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
 
   menu = [
-    { title: 'Panel',         icono: 'iconoir-home-simple',   url: '/dashboard/panel',  },
-    { title: 'Citas',         icono: 'iconoir-archive',       url: '/dashboard/citas',  },
-    { title: 'Crear Cita',    icono: 'iconoir-add-page',      url: '/dashboard/crear-citas',  },
-    { title: 'Usuarios',      icono: 'iconoir-group',         url: '/dashboard/usuarios',  },
-    { title: 'Categorias',    icono: 'iconoir-drawer',        url: '/dashboard/categorias',  },
-    { title: 'Descuentos',    icono: 'iconoir-money-square',  url: '/dashboard/descuentos'},
-    { title: 'Cerrar sesión', icono: 'iconoir-log-out',       url: '/dashboard/',  },
+    { title: 'Panel',           icono: 'iconoir-home-simple',   url: '/dashboard/panel',  },
+    { title: 'Consultar Cita',  icono: 'iconoir-archive',       url: '/dashboard/citas',  },
+    { title: 'Crear Cita',      icono: 'iconoir-add-page',      url: '/dashboard/crear-citas',  },
+    { title: 'Usuarios',        icono: 'iconoir-group',         url: '/dashboard/usuarios',  },
+    { title: 'Categorias',      icono: 'iconoir-drawer',        url: '/dashboard/categorias',  },
+    { title: 'Descuentos',      icono: 'iconoir-money-square',  url: '/dashboard/descuentos'},
+    { title: 'Cerrar sesión',   icono: 'iconoir-log-out',       url: '/dashboard/',  },
   ];
 
   constructor() { }
