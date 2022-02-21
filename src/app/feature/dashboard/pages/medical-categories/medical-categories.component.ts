05da00d550126f5d3d2f752e8122b5cb31c3c53c
Andrés Fabian Patiño Bermudez - Ceiba Software <andres.patino@ceiba.com.co>
2022-02-21 17:19:10 -0500

    nuevos componentes del sistema

--- src/app/feature/dashboard/pages/medical-categories/medical-categories.component.ts
+++ src/app/feature/dashboard/pages/medical-categories/medical-categories.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { MEDICALCATEGORIES } from '../../shared/mockup/medical-category.mockup';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalGlobalService } from '@core/services/modal-global.service';
+import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription } from 'rxjs';
 import { CreateMedicalCategorieComponent } from '../../components/create-medical-categorie/create-medical-categorie.component';
+import { MedicalCategory } from '../../shared/interfaces/medical-category.interface';
+import { MedicalCategoryService } from '../../shared/service/medical-category.service';
 
 @Component({
   selector: 'app-medical-categories',
@@ -10,17 +13,47 @@ import { CreateMedicalCategorieComponent } from '../../components/create-medical
 })
 export class MedicalCategoriesComponent implements OnInit {
 
-  medicalCategories = MEDICALCATEGORIES;
+  listSubscribers: Subscription[] = [];
+  medicalCategories: MedicalCategory[];
+  active = 1;
 
-  constructor(private modalService: NgbModal) { }
-
-  open() {
-    const modalRef = this.modalService.open(CreateMedicalCategorieComponent);
-    modalRef.componentInstance.name = 'World';
+  constructor(
+    private modalService: NgbModal,
+    private modalGlobalService: ModalGlobalService,
+    private medicalCategoriesService: MedicalCategoryService,
+    config: NgbModalConfig
+  ) {
+    config.backdrop = 'static';
+    config.keyboard = false;
   }
 
   ngOnInit(): void {
+    this.loadMedicalCategories();
+    this.listenObserver();
+  }
 
+  open() {
+    const modalRef = this.modalService.open(CreateMedicalCategorieComponent, { size: 'lg' });
+    modalRef.componentInstance.name = 'Crear Categoria Medica';
+  }
+
+  loadMedicalCategories() {
+    this.medicalCategoriesService.getAllCategories()
+      .subscribe((data: MedicalCategory[]) => {
+        this.medicalCategories = data;
+      })
+  }
+
+  listenObserver = () => {
+    const observer1$ = this.modalGlobalService.event.subscribe(() => {
+      this.loadMedicalCategories();
+    })
+
+    this.listSubscribers = [observer1$];
+  }
+
+  ngOnDestroy(): void {
+    this.listSubscribers.map(a => a.unsubscribe());
   }
 
 }
--- src/app/feature/dashboard/pages/users/users.component.ts
+++ src/app/feature/dashboard/pages/users/users.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { USERS } from '../../shared/mockup/users.mockup';
+import { ModalGlobalService } from '@core/services/modal-global.service';
+import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription } from 'rxjs';
+import { CreateUserComponent } from '../../components/create-user/create-user.component';
+import { User } from '../../shared/interfaces/user.interface';
+import { UserService } from '../../shared/service/user.service';
 
 @Component({
   selector: 'app-users',
@@ -8,11 +13,47 @@ import { USERS } from '../../shared/mockup/users.mockup';
 })
 export class UsersComponent implements OnInit {
 
-  users = USERS;
 
-  constructor() { }
+  users: User[];
+  listSubscribers: Subscription[] = [];
+
+  constructor(
+    private modalService: NgbModal,
+    private userService: UserService,
+    private modalGlobalService: ModalGlobalService,
+    config: NgbModalConfig) {
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
+
 
   ngOnInit(): void {
+    this.loadUsers();
+    this.listenObserver();
+  }
+
+  createUserModal() {
+    const modalRef = this.modalService.open(CreateUserComponent, {size: 'lg'})
+    modalRef.componentInstance.name = 'Crear Usuario';
+  }
+
+  loadUsers() {
+    this.userService.getAll()
+      .subscribe((data: User[]) => {
+        this.users = data;
+      })
+  }
+
+  listenObserver = () => {
+    const observer1$ = this.modalGlobalService.event.subscribe(() => {
+      this.loadUsers();
+    })
+
+    this.listSubscribers = [observer1$];
+  }
+
+  ngOnDestroy(): void {
+    this.listSubscribers.map(a => a.unsubscribe());
   }
 
 }
