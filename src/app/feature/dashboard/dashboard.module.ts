9027617d2b39b2002a21b6b5273dac521154e085
Andrés Fabian Patiño Bermudez - Ceiba Software <andres.patino@ceiba.com.co>
2022-02-21 17:27:28 -0500

    agregados rutas y componentes para funcionamiento

--- src/app/feature/dashboard/dashboard.module.ts
+++ src/app/feature/dashboard/dashboard.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DashboardRoutingModule } from './dashboard.routing';
 
 
@@ -18,7 +18,11 @@ import { MedicalCategoriesComponent } from './pages/medical-categories/medical-c
 import { CreateMedicalCategorieComponent } from './components/create-medical-categorie/create-medical-categorie.component';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { CreateChargeComponent } from './components/create-charge/create-charge.component';
-
+import { CreateUserComponent } from './components/create-user/create-user.component';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { DoctorComponent } from './pages/doctor/doctor.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
 
 @NgModule({
   declarations: [
@@ -33,15 +37,21 @@ import { CreateChargeComponent } from './components/create-charge/create-charge.
     HealthCompanyComponent,
     MedicalCategoriesComponent,
     CreateMedicalCategorieComponent,
-    CreateChargeComponent
+    CreateChargeComponent,
+    CreateUserComponent,
+    DoctorComponent,
+    CategoriesComponent
   ],
   imports: [
     CommonModule,
     RouterModule,
     CoreModule,
     FormsModule,
+    ReactiveFormsModule,
     DashboardRoutingModule,
     BsDatepickerModule.forRoot(),
+    AlertModule.forRoot(),
+    TabsModule.forRoot(),
 
   ]
 })
--- src/app/feature/dashboard/dashboard.routing.ts
+++ src/app/feature/dashboard/dashboard.routing.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { AppoinmentsComponent } from './pages/appoinments/appoinments.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
 import { CreateAppoinmentComponent } from './pages/create-appoinment/create-appoinment.component';
 import { DiscountComponent } from './pages/discount/discount.component';
-import { MedicalCategoriesComponent } from './pages/medical-categories/medical-categories.component';
 import { PanelComponent } from './pages/panel/panel.component';
 import { UsersComponent } from './pages/users/users.component';
 
@@ -18,7 +18,7 @@ const routes: Routes = [
       { path: 'citas', component: AppoinmentsComponent },
       { path: 'crear-citas', component: CreateAppoinmentComponent },
       { path: 'descuentos', component: DiscountComponent },
-      { path: 'categorias', component: MedicalCategoriesComponent }
+      { path: 'categorias', component: CategoriesComponent }
 
     ]
   }
