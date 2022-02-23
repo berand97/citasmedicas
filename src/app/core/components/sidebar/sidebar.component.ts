a9d6bfdb48a6cb850a3b8bede732d3430d939c8f
Andrés Fabian Patiño Bermudez - Ceiba Software <andres.patino@ceiba.com.co>
2022-02-23 13:23:24 -0500

    cambios en el menu

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -13,7 +13,7 @@ export class SidebarComponent implements OnInit {
     { title: 'Crear Cita',      icono: 'iconoir-add-page',      url: '/dashboard/crear-citas',  },
     { title: 'Usuarios',        icono: 'iconoir-group',         url: '/dashboard/usuarios',  },
     { title: 'Categorias',      icono: 'iconoir-drawer',        url: '/dashboard/categorias',  },
-    { title: 'Descuentos',      icono: 'iconoir-money-square',  url: '/dashboard/descuentos'},
+    /* { title: 'Descuentos',      icono: 'iconoir-money-square',  url: '/dashboard/descuentos'}, */
     { title: 'Cerrar sesión',   icono: 'iconoir-log-out',       url: '/dashboard/',  },
   ];
 
