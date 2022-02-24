fb1e90ae368d1361723b011faf470747d1b63eee
Andrés Fabian Patiño Bermudez - Ceiba Software <andres.patino@ceiba.com.co>
2022-02-24 18:05:06 -0500

    cambios en las interfaces

---
+++ src/app/feature/dashboard/shared/interfaces/appoinment.interface.ts
@@ -0,0 +1,15 @@
+export interface Appoinment {
+  id: number;
+  name: string;
+  lastName: string;
+  typeDocument: string;
+  numberDocument: number;
+  healtyCompany: string;
+  typeAffiliate: string;
+  priceCategory: number;
+  discount: string;
+  hour: null;
+  categoryMedicalName: string;
+  appoinmentDate: string;
+  total: number;
+}
