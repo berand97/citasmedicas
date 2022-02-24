46b78edf6a6e7fdd77fc5bccb909f78f8f343631
Andrés Fabian Patiño Bermudez - Ceiba Software <andres.patino@ceiba.com.co>
2022-02-24 18:07:30 -0500

    preparacion de modelos y servicios de notificacion

--- src/app/core/services/notification.service.ts
+++ src/app/core/services/notification.service.ts
@@ -1,11 +1,47 @@
 import { Injectable } from '@angular/core';
+import { from, Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class NotificationService {
+  error(titulo: string) {
+    Swal.fire({
+      html: 'Ocurrio un error inesperado, por favor contacte al administrador',
+      title: titulo,
+      icon: 'error',
+      showCloseButton: true,
+      showConfirmButton: false,
+      cancelButtonText: 'Cerrar'
+    });
+  }
 
-  constructor() { }
+  sucess(title: string): Observable<ConfirmAction> {
+    return from(
+      Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: `Se ha creado ${title} satisfactoriamente`,
+        showConfirmButton: false,
+        timer: 3000
+      })
+    ).pipe(switchMap(a => of({ confirm: a.isConfirmed })));
+  }
 
+  warning() {
+    Swal.fire({
+      title: 'complete todos los campos',
+      icon: 'warning',
+      showConfirmButton: false,
+      timer: 3000
+    });
+  }
+
+}
+
+export interface ConfirmAction {
+  confirm: boolean;
 }
