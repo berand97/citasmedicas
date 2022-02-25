fc3d0d53138241527e9a85eceff414a798f5ba10
Andrés Fabian Patiño Bermudez - Ceiba Software <andres.patino@ceiba.com.co>
2022-02-25 13:37:56 -0500

    agregados caption en las tables

---
+++ src/app/shared/utils/mocks/appointment.mock.ts
@@ -0,0 +1,19 @@
+import { Appoinment } from "src/app/feature/dashboard/shared/interfaces/appoinment.interface";
+
+export const MockAppoinment: Appoinment[] = [
+  {
+    id: 1,
+    name: 'Karen',
+    lastName: 'Barrera',
+    typeDocument: 'Cedula de ciudadania',
+    numberDocument: 1118555894,
+    healtyCompany: 'Sura',
+    typeAffiliate: 'Contributivo',
+    priceCategory: 100000,
+    discount: '0.5',
+    hour: '15:00 A.M - 16:00 P.M',
+    categoryMedicalName: 'Optometría',
+    appoinmentDate: '2022-02-28',
+    total: 50000
+  }
+];
